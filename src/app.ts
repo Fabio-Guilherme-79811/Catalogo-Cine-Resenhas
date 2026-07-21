3f4f94ddd5b56244dd33f7ecaa8423a341ae617d
Oséias <oseiasfilho2009@gmail.com>
2026-07-21 17:43:11 -0300

    feat: Finalização app.ts servidor

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,16 @@ import express, {Request,Response, NextFunction}  from 'express';
 const app = express();
 const port = 3000;
 
-app.get ('/',(req:Request, res:Response, nex:NextFunction) =>{
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+
+app.get ('/',(req:Request, res:Response, next:NextFunction) =>{
     res.send('Typesscript & Express rodando lizin');
 });
 
+//================================== SERVER ========================================
 app.listen(port, () =>{
-    console.log(`Servidor ativo na porta &{port}``)
+    console.log(`Servidor ativo na porta http://localhost&{port}`)
 });
