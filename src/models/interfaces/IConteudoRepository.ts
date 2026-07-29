316684510c0bba41b55d5051bc7b3bd8459c8792
JF-hash <joaofelipebezerra34@gmail.com>
2026-07-29 11:28:50 -0300

    test: Correção de erros aparentes

--- src/models/interfaces/IConteudoRepository.ts
+++ src/models/interfaces/IConteudoRepository.ts
@@ -1,9 +1,9 @@
-import { Conteudo } from '../../entities/Conteudo';
+import { Conteudo } from '../../entities/conteudo';
 
 export interface IConteudoRepository {
     listarTodos(): Promise<Conteudo[]>;
     buscarPorId(id: string): Promise<Conteudo | null>;
-    buscarPorGenero(generoId: string): Promise<Conteudo[]>;
+    buscarPorGenero(genero: string): Promise<Conteudo[]>;
     criar(filme: Conteudo): Promise<Conteudo>;
     atualizar(id: string, filme: Conteudo): Promise<Conteudo | null>;
     remover(id: string): Promise<boolean>;
